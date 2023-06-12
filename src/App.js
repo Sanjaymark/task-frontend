0e935048e4e62b2a399ffa0e08d1665bc3fdd491
Sanjaymark16 <sanjaymark16@gmail.com>
2023-06-12 11:01:45 +0530

    project day1

--- src/App.js
+++ src/App.js
@@ -1,79 +1,41 @@
 
 import './App.css';
 import React, { useState } from 'react';
-import StudentCard from './components/student';
-import { StateFulCom, StatelessComp } from './components/StateComponent';
-import Button from './components/Button';
+import TopBar from './Components/TopBar';
+import Base from './BasePage/Base';
+import Dashboard from './Pages/Dashboard';
+import Profile from './Pages/Profile';
+
 
 function App() {
 
-  const [acceptCount, setAcceptCount] = useState(0)
-  const studentsData = [
-    {
-      name:"Selvam",
-      batch:"B47WD",
-      education:"B.E",
-    },
-    {
-      name:"Sanjay",
-      batch:"B45WD",
-      education:"B.E",
-    },
-    {
-      name:"Baskar",
-      batch:"B43WD",
-      education:"BCA",
-    },
-    {
-      name:"Naveen",
-      batch:"B42WD",
-      education:"BSC",
-    },
-  ]
+
+const studentData = [
+  {
+  name : "Sanjay",
+  age : 25
+  },
+  {
+    name : "Ajay",
+    age : 23
+    }
+]
+const [data, setData] = useState(studentData)
   return (
 <div className="App">
-<div>
-  <h1>Accepted Students : {acceptCount}</h1>
+  <Dashboard
+   data ={data}
+   setData ={setData}
+  />
 </div>
-<div className='card-block'>
-  {studentsData.map((stud,idx)=>(
-     <StudentCard
-     key={idx}
-     name={stud.name}
-     batch={stud.batch}
-     education={stud.education}
-     acceptCount={acceptCount}
-     setAcceptCount={setAcceptCount}
-     />
-  ))}
-  <div>
-    {/* <StateFulCom/>
-    <StatelessComp name={"Sanjay"}/> */}
-
-    <Button
-    background={"crimson"}
-    color="white"
-    pd="15px"
-    m="5px"
-    wd="max-content"
-    onClickFunc={()=>console.log("custom-btn clicked")}> Add </Button>
-
-  </div>
-</div>
-</div>
-  );
+  ); 
 }
-// const props={} => 
-// props= {
-//   name:"",
-//    batch:"", 
-//    education:""}
-//destructuring 
-// const {name, batch, education} = props
+
 
 export default App;
 
 
 
-
+// useState() - day1
+// useEffect() -
 
